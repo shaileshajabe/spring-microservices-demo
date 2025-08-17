57123a431a2abb6a7f8ead2a02e627a3c3c30f7d
Shailesh Ajabe <shaileshajabe2013@gmail.com>
2025-08-17 15:11:10 +0530

    First commit

--- Eureka-Server/src/main/java/com.example.eurekaserver/EurekaServerApplication.java
+++ Eureka-Server/src/main/java/com.example.eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.serviceregistry;
+package com.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceRegistryApplication {
+public class EurekaServerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRegistryApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
